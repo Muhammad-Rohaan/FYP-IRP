508261f397078b01dc04117dc225c65e28fd20e4
Muhammad-Rohaan <rohaanali82005@gmail.com>
2026-02-15 17:50:03 +0500

    Cookies Implemented | Authentication Done.

--- InstituteResourcePlanning/src/main/java/com/ProjectIRP/InstituteResourcePlanning/Filters/JwtFilter.java
+++ InstituteResourcePlanning/src/main/java/com/ProjectIRP/InstituteResourcePlanning/Filters/JwtFilter.java
@@ -4,6 +4,7 @@ import com.ProjectIRP.InstituteResourcePlanning.Services.IrpUserDetailsService;
 import com.ProjectIRP.InstituteResourcePlanning.Utilities.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -31,29 +32,80 @@ public class JwtFilter extends OncePerRequestFilter {
         this.irpUserDetailsService = irpUserDetailsService;
     }
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authHeader = request.getHeader("Authorization");
 
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            String token = authHeader.substring(7);
-            String userEmail = jwtUtil.getUserEmailFromJwtToken(token);
+/// Working properly before Cookies implementation.
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+//        String authHeader = request.getHeader("Authorization");
+//
+//        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+//            String token = authHeader.substring(7);
+//            String userEmail = jwtUtil.getUserEmailFromJwtToken(token);
+//
+//            if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+//                UserDetails userDetails = irpUserDetailsService.loadUserByUsername(userEmail);
+////                System.out.println("doFilterInternal → userDetails.getAuthorities() "+userDetails.getAuthorities());
+//
+//                if (jwtUtil.validateJwtToken(token, userDetails)) {
+////                    System.out.println("doFilterInternal → userDetails.getUsername() "+userDetails.getUsername());
+//                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+//                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+//                    SecurityContextHolder.getContext().setAuthentication(authToken);
+////                    System.out.println("OK");
+////                    System.out.println("AUTH HEADER = " + authHeader);
+////                    System.out.println("TOKEN = " + token);
+//                }
+//            }
+//        }
+//        filterChain.doFilter(request, response);
+//    }
 
-            if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails = irpUserDetailsService.loadUserByUsername(userEmail);
+
+///  for cookies implementation
+@Override
+protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    String authHeader = request.getHeader("Authorization");
+    String token = getJwtTokenFromCookies(request);
+
+    if (token != null) {
+//        String token = authHeader.substring(7);
+        String userEmail = jwtUtil.getUserEmailFromJwtToken(token);
+
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = irpUserDetailsService.loadUserByUsername(userEmail);
 //                System.out.println("doFilterInternal → userDetails.getAuthorities() "+userDetails.getAuthorities());
 
-                if (jwtUtil.validateJwtToken(token, userDetails)) {
+            if (jwtUtil.validateJwtToken(token, userDetails)) {
 //                    System.out.println("doFilterInternal → userDetails.getUsername() "+userDetails.getUsername());
-                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
 //                    System.out.println("OK");
 //                    System.out.println("AUTH HEADER = " + authHeader);
 //                    System.out.println("TOKEN = " + token);
-                }
             }
         }
-        filterChain.doFilter(request, response);
     }
+    filterChain.doFilter(request, response);
+}
+
+
+    public String getJwtTokenFromCookies(HttpServletRequest httpServletRequest) {
+        if (httpServletRequest.getCookies() == null) {
+            return null;
+        }
+
+        for (Cookie cookie: httpServletRequest.getCookies()) {
+            if ("jwt_token".equals(cookie.getName())) {
+                return cookie.getValue();
+            }
+        }
+        return "JWT token not found in cookies";
+    }
+
+
+
+
+
+
 }
